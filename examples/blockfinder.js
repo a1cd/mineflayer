e5157d7d2d70c1c9bc85cafc47d1a61bfade0128
a1cd <71281043+a1cd@users.noreply.github.com>
2021-06-01 08:51:13 -0400

    made count 500
    made count 500 to test things

--- examples/blockfinder.js
+++ examples/blockfinder.js
@@ -37,7 +37,7 @@ bot.on('chat', async (username, message) => {
     const ids = [mcData.blocksByName[name].id]
 
     const startTime = performance.now()
-    const blocks = bot.findBlocks({ matching: ids, maxDistance: 128, count: 10 })
+    const blocks = bot.findBlocks({ matching: ids, maxDistance: 128, count: 500 })
     const time = (performance.now() - startTime).toFixed(2)
 
     bot.chat(`I found ${blocks.length} ${name} blocks in ${time} ms`)
